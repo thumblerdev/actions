8e42cbc62a1a088cd046696bff09b025745e01df
Mohamed Hamza <manwar@aquilaos.com>
2022-02-13 13:18:40 +0100

    update

--- docker/build/index.js
+++ docker/build/index.js
@@ -1,15 +1,13 @@
 'use strict';
 
 const core = require('@actions/core');
+const artifact = require('@actions/artifact').create();
 const github = require('@actions/github');
 const parser = require('action-input-parser');
 const exec = require('child_process').exec;
+const path = require('path');
 
-try {
-  const tags = parser.getInput('tags', {type: 'array'});
-  const context = parser.getInput('context');
-  const dockerfile = parser.getInput('dockerfile');
-
+async function buildImage(dockerfile, context, tags) {
   const command = `
     docker build \
       ${tags.map(tag => '-t ' + tag).join(' ')} \
@@ -17,15 +15,27 @@ try {
       -f ${dockerfile}
   `;
 
-  console.log(command);
-
-  exec(command, (err, stdout, stderr) => {
-    console.log(stdout);
-    console.error(stderr);
-    if (err) {
-      core.setFailed(err);
-    }
-  })
-} catch (error) {
-  core.setFailed(error.message);
+  return exec(command);
 }
+
+async function exportImage(imageName, fileName) {
+  const command = `docker save ${imageName} -o ${fileName}`;
+
+  return exec(command);
+}
+
+async function main() {
+  try {
+    const tags = parser.getInput('tags', { type: 'array' });
+    const context = parser.getInput('context');
+    const dockerfile = parser.getInput('dockerfile');
+    const imageName = path.basename(tags[0]);
+
+    await buildImage(dockerfile, context, tags);
+    await exportImage(imageName, imageName + '.tgz');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
